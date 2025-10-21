cca2e0dae8425bb78ff01b466e132bd5a69c98bd
manemajef <rotem.semah@gmail.com>
2025-10-21 17:13:21 +0300

    waves

--- src/wave.c
+++ src/wave.c
@@ -4,19 +4,53 @@
 #define RATE 441
 
 
+double *get_samples() {
+  double *res = (double*)malloc(sizeof(double) * RATE);
+  if (!res) {
+    printf("error, exiting\n");
+  }
+  return res;
+}
+
 
 double *sine_wave(int freq) {
-double phase = 0.0;
+  double dur_sec = 1.0;
+  double phase = 0.0;
   double diff = (double)freq / RATE;
   int total = dur_sec * RATE;
-  double *samples = (double *)malloc(sizeof(double) * total);
+  double *samples = get_samples();
   double *samp = samples;
   for (int i = 0; i < total; i++) {
     if (phase > 1) phase -= 1;
-    *samp = sin(phase * M_PI  * 2);
+    *(samp++) = sin(phase * M_PI  * 2);
+    phase += diff;
+
+  }
+  return samples;
+}
+
+double *saw_wave(int freq) {
+  double phase = -1.0;
+  double diff = (double)freq/ RATE;
+  double *samples = get_samples();
+  double *samp = samples;
+  for (int i = 0; i< RATE; i++) {
+    if (phase >= 1) phase -= 2.0;
+    *(samp++) = phase;
     phase += diff;
   }
   return samples;
 }
 
-double
+double *sqaure_wave(int freq) {
+  double phase = 1.0;
+  double diff = (double) freq/RATE;
+  double *samples = get_samples();
+  double *samp = samples;
+  for (int i = 0; i < RATE; i++){
+    if (phase > 1) phase --;
+    *(samp++) = phase > 0.5 ? 1.0 : -1.0;
+    phase += diff;
+  }
+  return samples;
+}
